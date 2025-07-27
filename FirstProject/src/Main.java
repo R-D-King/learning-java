4b03b426d0839009f6042314187a69662f06f2f0
King <24613526+R-D-King@users.noreply.github.com>
2025-07-27 03:42:45 +0300

    Initial commit + first small project Compound interest calculator

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Initializing a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Initializing Variables
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+
+        // Taking user's input
+        System.out.print("Enter principal: ");
+        principal = input.nextDouble();
+
+        System.out.print("Enter rate (%): ");
+        rate = input.nextDouble() / 100;
+
+        System.out.print("Enter the Number of Times Compounded Per Year: ");
+        timesCompounded = input.nextInt();
+
+        System.out.print("Enter the Number of Years: ");
+        years = input.nextInt();
+
+
+        // Calculating the interest
+        amount = principal * Math.pow(1 + (rate / timesCompounded), timesCompounded * years);
+        System.out.printf("Your amount after %d year/s is $%f\n", years, amount);
+
+        
+        // Closing the Scanner
+        input.close();
+    }
+}
